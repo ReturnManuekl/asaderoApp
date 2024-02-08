98c0683bd0e96ceb46ea27cc2b1cbb940f147364
Manuel <manuel.zerg@gmail.com>
2024-02-08 15:21:39 -0600

    I create the login page in index

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php include_once('components/header.php'); ?>
-<div class="container"> Bienvenido</div>
-<?php include_once('components/footer.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-naranja d-flex flex-column align-items-center vh-100">
+        <h1 class="text-center m-5 text-light">Pollos</br>El asadero</h1>
+        <div class="bg-white form-contenedor p-3 rounded-4">
+            <form action="">
+                <p class="texto">Usuario:</p>
+                <input type="text" class="rounded mb-4 bg-gris border-0 w-100">
+                <p class="texto">Contraseña:</p>
+                <input type="password" class="rounded mb-4 bg-gris border-0 w-100"></br>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary lef">Entrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
+
